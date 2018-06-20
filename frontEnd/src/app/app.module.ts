827adc0b781db51613f67343860f7c42d8aeba80
Magno Carvalho <magnocarv@hotmail.com>
2018-06-20 20:14:23 -0300

    app module e login component atualizados

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -4,8 +4,9 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatSidenavModule, MatListModule, MatNativeDateModule, MatToolbarModule, MatIconModule, MatCardModule, MatFormFieldModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatSidenavModule, MatListModule, MatNativeDateModule, MatToolbarModule, MatIconModule, MatCardModule, MatInputModule } from '@angular/material';
 import { MenuComponent } from './menu/menu.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { MenuComponent } from './menu/menu.component';
     MatNativeDateModule,
     MatListModule,
     MatSidenavModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
