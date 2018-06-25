c78175bec5cb6b358971ae216ef7a7ce40ae5bdd
Magno Carvalho <magnocarv@hotmail.com>
2018-06-25 16:56:15 -0300

    commit ajustes front

--- backEnd/src/routes/api.ts
+++ backEnd/src/routes/api.ts
@@ -12,5 +12,6 @@ router.post('/salvarFotos', PhotoCtlr.putPhotos);
 router.get('/carregarAlbuns/:id',PhotoCtlr.buscarAlbuns);
 router.get('/carregarFotos/:id',PhotoCtlr.buscarAlbum);
 router.post('/apagarFoto', PhotoCtlr.deletarFoto);
+router.post('/addFotos', PhotoCtlr.addFotos);
 
 export = router;
